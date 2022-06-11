42a0526bb59146c0c4660147177bcfb70ab5a35c
jcpenfie <juancarlospfierro@gmail.com>
2022-06-11 19:10:58 +0200

    storeAs

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,7 +49,7 @@ class UserController extends Controller
                 //Nombre final de la imagen: quita todos los posibles espacios por guión bajos que tenga en el nombre, le añade un número random, la hora en milisegundos para que no se repita y la extensión de la imagen
                 $compPic = str_replace(' ', '_', $fileNameOnly) . '-' . rand() . '_' . time() . '.' . $extenshion;
 
-                $request->file('profileImg')->storePubliclyAs('img/logo', $compPic);
+                $request->file('profileImg')->storeAs('img/logo', $compPic);
                 User::where('userName', $request->userName)->update(array(
                     'profileImg' => $compPic,
                 ));
