076d7bd187d5cae3c8bf9079e509facd1d6fb883
jcpenfie <juancarlospfierro@gmail.com>
2022-06-11 18:52:10 +0200

    Cambios en la subida

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -56,7 +56,7 @@ class UserController extends Controller
                 //Nombre final de la imagen: quita todos los posibles espacios por guión bajos que tenga en el nombre, le añade un número random, la hora en milisegundos para que no se repita y la extensión de la imagen
                 $compPic = str_replace(' ', '_', $fileNameOnly) . '-' . rand() . '_' . time() . '.' . $extenshion;
 
-                $upload = $s3->upload($bucket, $completeFileName, fopen($compPic, 'rb'), 'public-read');
+                $upload = $s3->upload($bucket, $compPic, fopen($completeFileName, 'rb'), 'public-read');
                 User::where('userName', $request->userName)->update(array(
                     'profileImg' => $compPic,
                 ));
